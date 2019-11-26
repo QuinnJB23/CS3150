2c9e3fbbc7c3dc737d3d53625e9a9e49b11981af
Jason Quinn <quinnjb23@gmail.com>
2019-11-25 23:55:29 -0700

    finished lap function and worked on lap implementation

--- project/Stopwatch.cpp
+++ project/Stopwatch.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <ctime>
 #include <bits/stdc++.h>
 #include "Stopwatch.h"
@@ -13,7 +15,7 @@ void Stopwatch::start(int people)
 	{
 		cout << "Please type 'start' to start the stopwatch." << endl;
 		cin >> start;
-		
+
 		if (start == "start" || start == "Start")
 		{
 			started = 1;
@@ -23,20 +25,31 @@ void Stopwatch::start(int people)
 	double currentLap = 0;
 	int lapCount = 0;
 	bool quit = 0;
+	int person;
 	string in;
 	cout << "Stopwatch started" << endl;
+	cout << "Enter athlete number to record their lap or type 'all' to record all athletes laps" << endl;
 	while (!quit)
 	{
 		cin >> in;
 		if (in == "stop" || in == "Stop")
 		{
 			finish();
-		} else if (in == "lap" || in == "Lap")
+		} else if (in == "all" || in == "All")
 		{
+			lap(0, clockAtStart);
+		} else {
+			istringstream iss (in);
+			iss >> person;
+			if (iss.fail()) {
+				cout << "Uh-oh" << endl;
+				exit(1);
+			}
+			lap(person, clockAtStart);
 		}
 
-			currentLap++;
-			cout << "Lap time: ";
+		currentLap++;
+		cout << "Lap time: ";
 		cout << "Time taken in millisecs: " << clock() - clockAtStart << endl;
 		cout << "Time taken in seconds: " << (clock() - clockAtStart) / 1000;
 		cin.ignore();
@@ -46,10 +59,35 @@ void Stopwatch::start(int people)
 void Stopwatch::lap(int person, double clockAtStart)
 {
 	double split;
-	if (splits[person-1].size() == 1)
+
+	if (person == 0)
+	{
+		for(int i = 0; i < people; i++)
+		{
+			if (splits[i].size() == 1)
+			{
+				split = clock() - clockAtStart;
+				splits[i].push_back(split);
+				cout << split << endl;
+			} else
+			{
+				split = clock() - splits[person-1][0];
+				splits[i].push_back(split);
+				splits[i][0] += split;
+				cout << split << endl;
+			}
+		}
+	} else if (splits[person-1].size() == 1)
 	{
 		split = clock() - clockAtStart;
 		splits[person-1].push_back(split);
+		cout << split << endl;
+	} else
+	{
+		split = clock() - splits[person-1][0];
+		splits[person-1].push_back(split);
+		splits[person-1][0] += split;
+		cout << split << endl;
 	}
 }
 
